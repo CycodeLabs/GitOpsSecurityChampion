b4f755ffcceab2a2ca719d791e76f63ef2869d37
elad_pt <124190990+elad-pticha@users.noreply.github.com>
2024-11-17 10:11:43 -0700

    chore: changed base endpoint (#23)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,10 +12,10 @@ const config = {
   favicon: 'https://e5s6t7j5.rocketcdn.me/wp-content/uploads/2020/12/fav2.svg',
 
   // Set the production url of your site here
-  url: 'https://cycodelabs.github.io/',
+  url: 'https://gitopsecurity.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/GitOpsSecurityChampion',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
